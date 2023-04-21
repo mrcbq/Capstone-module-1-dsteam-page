92515e7a5283f78551dd3930f1a15f6faeee1aa8
mrcbq <marcobaquero@gmail.com>
2023-04-20 22:50:53 -0500

    Fix home page cards and services

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const courses = [
     logoSource: './images/codse.png',
     title: 'CODSE',
     subtitle: 'Certificación Certified Offensive and Defensive Security EXPERT',
-    description: "At the end of the course you must pass a certification exam, which consists of several 100% practical solution statements, related to a digital forensic investigation, of the type 'Capture The Flag' computer challenge.",
+    description: "Certification exam, consists of several 100% practical solution statements, related to a digital forensic investigation.",
   },
   {
     logoSource: './images/codse2.png',
     title: 'CODSE Xtreme',
     subtitle: 'Certificación Certified Offensive and Defensive Security Professional',
-    description: "Include new topics and attack and defense techniques such as: Pivoting, Lateral Movement, Aggressive escalation of privileges in Linux and Windows systems, SIEM event correlation systems, practical exercises.",
+    description: "Attack and defense techniques: Pivoting, Lateral Movement, Aggressive escalation of privileges in Linux and Windows systems.",
   },
   {
     logoSource: './images/kaliLogo.png',
     title: 'Kali Linux 2023',
     subtitle: 'Operating system base for the laboratories',
-    description: 'Exercises such as Capture The Flag, practical classes and proofs of concept, the operating system specialized in intrusion tests is used.',
+    description: 'Exercises such as Capture The Flag, practical classes and proofs of concept, system specialized in intrusion tests is used.',
   },
   {
     logoSource: './images/sec.png',
@@ -28,7 +28,7 @@ const courses = [
     logoSource: './images/wireshark.png',
     title: 'Network packet analysis',
     subtitle: 'Technique for diagnosing and solving problems in computer networks',
-    description: 'Analysis and manipulation of network packets. Network packet analysis techniques for identifying network problems and resolving security problems.',
+    description: 'Analysis and manipulation of network packets for identifying network problems and resolving security problems.',
   },
   {
     logoSource: './images/nmap.jpg',
@@ -43,9 +43,9 @@ const courseContainer = document.getElementById('courses-wrapper');
 courses.forEach((course, idx) => {
   const card = document.createElement('div');
   if (idx >= 2) {
-    card.className = 'card flex-row align-items-center p-2 border-0 course-card active-more-bnt';
+    card.className = 'card flex-row border-0 course-card active-more-bnt';
   } else {
-    card.className = 'card flex-row align-items-center p-2 border-0 course-card';
+    card.className = 'card flex-row border-0 course-card';
   }
 
   const imgContainer = document.createElement('div');
@@ -64,20 +64,20 @@ courses.forEach((course, idx) => {
   imgContainer.appendChild(imgLogo);
 
   const courseText = document.createElement('div');
-  courseText.className = 'card-course-text p-3';
+  courseText.className = 'card-course-text';
 
   const title = document.createElement('h5');
-  title.className = 'card-title p-1';
+  title.className = 'card-title';
   title.textContent = course.title;
   courseText.appendChild(title);
 
   const subtitle = document.createElement('h5');
-  subtitle.className = 'card-title p-1 color-red';
+  subtitle.className = 'card-title color-red';
   subtitle.textContent = course.subtitle;
   courseText.appendChild(subtitle);
 
   const description = document.createElement('p');
-  description.className = 'card-text p-1 color-black';
+  description.className = 'card-text color-black';
   description.textContent = course.description;
   courseText.appendChild(description);
 
